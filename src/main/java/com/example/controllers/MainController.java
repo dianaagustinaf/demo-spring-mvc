b0140dd8a91be8c13054d730720e7ac8e3924736
dianaagustinaf <dianaagustinaf@gmail.com>
2022-05-24 09:20:01 +0200

    Before Thymeleaf

--- src/main/java/com/example/controllers/MainController.java
+++ src/main/java/com/example/controllers/MainController.java
@@ -1,18 +1,38 @@
 package com.example.controllers;
 
+import com.example.services.EmpleadoService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping(value = "/")
 public class MainController {
-    
-    @GetMapping("/listar")
-    public String listarEmpleado() {
 
+    @Autowired
+    private EmpleadoService empleadoService;
+    
+    /*
+    @GetMapping("/listar")
+    public String listarEmpleado(Model model) {
+
+        model.addAttribute("empleados", empleadoService.findAll());
         return "listar";
     }
+    */
+    
+    @GetMapping("/listar")
+    public ModelAndView listarEmpleado(){
 
+        ModelAndView maw = new ModelAndView("listar");
+
+        maw.addObject("empleados", empleadoService.findAll());
+
+        return maw;
+    }
 
 }
